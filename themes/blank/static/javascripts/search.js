f7d71728f0b8c896c7ec38db48cb03c935ee2ef0
Peter Yates <pd.yates@gmail.com>
2018-05-11 13:56:40 +0100

    Add search via Algolia

---
+++ themes/blank/static/javascripts/search.js
@@ -0,0 +1,56 @@
+const options = {
+  appId: '6CTZTIP7UD',
+  apiKey: '2ac20de2c4e9354e3521a1db515f00c2',
+  indexName: 'prod_BH4',
+  earchOnEmptyQuery: false,
+  /*
+  searchFunction: function(helper) {
+    let searchResults = document.getElementById("hits");
+
+    if (helper.state.query === '') {
+      searchResults.style.display = "none";
+      return;
+    }
+
+    helper.search();
+    searchResults.style.display = "inline";
+  }
+  */
+};
+
+const search = instantsearch(options);
+
+// initialize SearchBox
+search.addWidget(
+  instantsearch.widgets.searchBox({
+    container: '#search-box',
+    placeholder: 'Search for services',
+    magnifier: false
+  })
+);
+
+// initialize hits widget
+search.addWidget(
+  instantsearch.widgets.hits({
+    container: '#hits',
+
+    transformData: {
+      item: function(original) {
+        original.tags_snakecase = original.tags.map(tag => {
+          return tag
+            .toLowerCase()
+            .replace(/ /g,'-')
+            .replace(/[^\w-]+/g,'')
+        });
+        return original;
+      }
+    },
+
+    templates: {
+      empty: 'No results',
+      item: '<li class="card"><a href="{{uri}}">{{{_highlightResult.title.value}}}</a> <ul class="tags">{{#tags_snakecase}}<li class="tag"><a class="button button-tag" href="/tags/{{.}}">{{{.}}}</a></li>{{/tags_snakecase}}</ul>{{{_highlightResult.content.value}}}</li>'
+    }
+  })
+);
+
+search.start();
